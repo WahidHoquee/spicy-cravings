43b65315dcd0ee366edcf25bf6e5668780d8a4b5
WahidHoquee <wahidhoquee@gmail.com>
2023-08-19 15:01:40 +0600

    Added home page

---
+++ components/Home.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <img src="/recipe.jpg" alt="Cover Photo" />
+      <div className="home-body">
+        <h2>Subscribe Youtube Now</h2>
+        {/* <h2>Spicy Cravings</h2> */}
+        <p>
+          কুপনটি সংগ্রহ করতে ইউটিউব চ্যানেলটি সাবস্ক্রাইব করুন। এবং ওয়েলকাম ফুড
+          বুঝে নিন।
+        </p>
+        <button>Get Coupon</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ components/Loader.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+// import './Loader.scss'
+
+const Loader = () => {
+  return (
+    <div className='splash'>
+      <img src='/logo.png' alt="Spicy Cravings"/>
+    </div>
+  )
+}
+
+export default Loader
