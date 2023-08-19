a86895cffd44f69772a72656a3bf43489d431006
WahidHoquee <wahidhoquee@gmail.com>
2023-08-19 16:13:18 +0600

    UI Design done

--- components/Home.tsx
+++ components/Home.tsx
@@ -6,13 +6,13 @@ const Home = () => {
     <div className="home">
       <img src="/recipe.jpg" alt="Cover Photo" />
       <div className="home-body">
-        <h2>Subscribe Youtube Now</h2>
-        {/* <h2>Spicy Cravings</h2> */}
+        {/* <h2>Subscribe Youtube Now</h2> */}
+        <h2>Spicy Cravings</h2>
         <p>
           কুপনটি সংগ্রহ করতে ইউটিউব চ্যানেলটি সাবস্ক্রাইব করুন। এবং ওয়েলকাম ফুড
           বুঝে নিন।
         </p>
-        <button>Get Coupon</button>
+        <a href="https://www.youtube.com/@spicycravings4113">Get Coupon</a>
       </div>
     </div>
   );
